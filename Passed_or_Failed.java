b890ba642ad1c9ed309655e4d28e8eada2b8c799
22a91a61j6 <121783995+22a91a61j6@users.noreply.github.com>
2023-09-07 21:30:13 +0530

    import java.util.*; public class Eww{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         int d=sc.nextInt();         int e=sc.nextInt();         if(a>34 && b>34 && c>34 && d>34 && e>34)         {              System.out.print("PASSED");          }else{              System.out.print("FAILED");          }     } } // import java.util.*; // public class Me{ //     public static void main(String[] args) { //         Scanner sc=new Scanner(System.in); //         int[] arr=new int[5]; //         int n=sc.nextInt(); //         ijn //         //for(int i=1;i<=n;i++) //         if(arr[i]>=34){ //             System.out.print("PASSED"); //             i++; //         }else{ //             System.out.print("FAILED"); //         } //     } // }

---
+++ Passed_or_Failed.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Eww{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=sc.nextInt();
+        int e=sc.nextInt();
+        if(a>34 && b>34 && c>34 && d>34 && e>34)
+        {
+             System.out.print("PASSED");
+         }else{
+             System.out.print("FAILED");
+         }
+    }
+}
+// import java.util.*;
+// public class Me{
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//         int[] arr=new int[5];
+//         int n=sc.nextInt();
+//         ijn
+//         //for(int i=1;i<=n;i++)
+//         if(arr[i]>=34){
+//             System.out.print("PASSED");
+//             i++;
+//         }else{
+//             System.out.print("FAILED");
+//         }
+//     }
+// }
