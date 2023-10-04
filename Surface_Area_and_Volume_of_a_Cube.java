91a7fb7334c6ddf20bd6335929ccbea4819d393b
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-04 21:10:42 +0530

    import java.util.*; public class Test{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int s=sc.nextInt();         int n=6*s*s;         int m=s*s*s;         System.out.println("Surface area = "+n+" and Volume = "+m);     } }

---
+++ Surface_Area_and_Volume_of_a_Cube.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int n=6*s*s;
+        int m=s*s*s;
+        System.out.println("Surface area = "+n+" and Volume = "+m);
+    }
+}
