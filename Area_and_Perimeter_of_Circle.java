96a317cf8e50a4c01e48f01dda58222e07951a24
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-04 20:58:49 +0530

    import java.util.*; public class Test{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         double a=n*n*3.14;         double p=2*3.14*n;         System.out.printf("%.2f",a);         System.out.println();         System.out.printf("%.2f",p);     } }

--- Area_and_Perimeter_of_Circle.java
+++ Area_and_Perimeter_of_Circle.java
@@ -1,12 +1,12 @@
 import java.util.*;
 public class Test{
-    public static void main(String []args){
+    public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
-        double a=3.14*n*n;
-        double b=3.14*2*n;
+        double a=n*n*3.14;
+        double p=2*3.14*n;
         System.out.printf("%.2f",a);
         System.out.println();
-        System.out.printf("%.2f",b);
+        System.out.printf("%.2f",p);
     }
 }
