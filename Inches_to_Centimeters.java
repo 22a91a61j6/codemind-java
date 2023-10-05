06d01be03bf1b4eb344c430ee65c34cfc519c6e3
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-05 11:07:55 +0530

    import java.util.*; public class Test{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         int c=sc.nextInt();         double m=c*2.54;         System.out.printf("%.2f",m);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Test{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int c=sc.nextInt();
+        double m=c*2.54;
+        System.out.printf("%.2f",m);
+    }
+}
