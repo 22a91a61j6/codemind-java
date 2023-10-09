103f3ffa3b476bda2c221d556c67430c1b2c038f
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-09 10:45:15 +0530

    import java.util.*; public class Test{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int s=sc.nextInt();         int b=sc.nextInt();         int a=s*b/2;         System.out.println(a);     } }

---
+++ Area_of_a_triangle_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int s=sc.nextInt();
+        int b=sc.nextInt();
+        int a=s*b/2;
+        System.out.println(a);
+    }
+}
