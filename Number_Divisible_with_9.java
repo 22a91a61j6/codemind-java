bce6714d24b049891210532927ee66d75e95cca7
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-10 19:05:19 +0530

    import java.util.*; public class test{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%9==0){             System.out.println("True");         }else{             System.out.println("False");         }     } }

---
+++ Number_Divisible_with_9.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class test{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%9==0){
+            System.out.println("True");
+        }else{
+            System.out.println("False");
+        }
+    }
+}
