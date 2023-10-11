8b6d50351e25a76dca8f01a9926a36d3c4fb30b7
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-11 07:50:51 +0530

    import java.util.*; public class Test{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n>20){             System.out.println("HOT");         }         else{             System.out.println("COLD");         }     } }

--- Climate.java
+++ Climate.java
@@ -2,8 +2,8 @@ import java.util.*;
 public class Test{
     public static void main(String []args){
         Scanner sc=new Scanner(System.in);
-        int s=sc.nextInt();
-        if(s>20){
+        int n=sc.nextInt();
+        if(n>20){
             System.out.println("HOT");
         }
         else{
