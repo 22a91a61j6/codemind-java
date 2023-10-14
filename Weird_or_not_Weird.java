787c63803be87b6ff8ab2c88e1887d6fc20fb198
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-14 10:06:37 +0530

    import java.util.*; public class test{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         if(n%2!=0){
                System.out.println("weird");
            }
            else if(n%2==0 && n>=2 && n<=5){
                System.out.println("not weird");
            }
            else if(n%2==0 && n>=6 && n<=20){
                System.out.println("weird");
            }
            else{
                System.out.println("not weird");
            }
        }
    }

---
+++ Weird_or_not_Weird.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class test{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2!=0){
+            
+            System.out.println("weird");
+        }
+        else if(n%2==0 && n>=2 && n<=5){
+            System.out.println("not weird");
+        }
+        else if(n%2==0 && n>=6 && n<=20){
+            System.out.println("weird");
+        }
+        else{
+            System.out.println("not weird");
+        }
+    }
+}
