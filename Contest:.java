e2eec7592faf4adb3c456fcdfe2712afdfbae80a
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2023-10-18 11:00:04 +0530

    import java.util.*; public class Test{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         if(a <= (b*1)+(c*2))         {             System.out.println("Qualify");         }else{             System.out.println("Not Qualify");         }     } }

--- Contest:.java
+++ Contest:.java
@@ -1,13 +1,11 @@
 import java.util.*;
-public class Me{
-    public static void main(String[] args) {
+public class Test{
+    public static void main(String []args){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
         int a=sc.nextInt();
         int b=sc.nextInt();
-        int c=a*1;
-        int d=b*2;
-        if((c+d) >= n)
+        int c=sc.nextInt();
+        if(a <= (b*1)+(c*2))
         {
             System.out.println("Qualify");
         }else{
