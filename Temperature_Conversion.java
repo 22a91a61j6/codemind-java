4d0fd15454734023fd838d9420f1efcd067a1675
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2024-03-03 20:25:33 +0530

    import java.util.*; public class Test{     public static void main(String []args){         Scanner sc=new Scanner (System.in);         int a=sc.nextInt();         double f=32+(a*1.8);         System.out.printf("%.2f",f);     } }

--- Temperature_Conversion.java
+++ Temperature_Conversion.java
@@ -1,12 +1,9 @@
 import java.util.*;
-public class Test
-{
+public class Test{
     public static void main(String []args){
-    
-        Scanner sc=new Scanner(System.in);
+        Scanner sc=new Scanner (System.in);
         int a=sc.nextInt();
-        double b;
-        b=32+(a*1.8);
-        System.out.printf("%.2f",b);
+        double f=32+(a*1.8);
+        System.out.printf("%.2f",f);
     }
 }
