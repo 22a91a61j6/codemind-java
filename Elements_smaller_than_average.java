656c74c7cb65120b1c90ec332b8911c68ac0c531
YELISETTI S R K SURYA CHANDRA RAJESH <121783995+22a91a61j6@users.noreply.github.com>
2024-03-20 18:28:16 +0530

    import java.util.*; public class Test{     public static void main(String []args){         Scanner sc=new Scanner (System.in);         int a=sc.nextInt();         int arr[]=new int[a];         int c=0;         for(int i=0;i<a;i++){             arr[i]=sc.nextInt();             c=c+arr[i];         }
            int b=c/a;
            int d=0;
            for(int i=0;i<a;i++){
                if(arr[i]<=b){
                    d=d+1;
                }
            }
            System.out.println(d);
        }
    }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Test{
+    public static void main(String []args){
+        Scanner sc=new Scanner (System.in);
+        int a=sc.nextInt();
+        int arr[]=new int[a];
+        int c=0;
+        for(int i=0;i<a;i++){
+            arr[i]=sc.nextInt();
+            c=c+arr[i];
+        }
+        
+        int b=c/a;
+        int d=0;
+        for(int i=0;i<a;i++){
+            if(arr[i]<=b){
+                d=d+1;
+            }
+        }
+        System.out.println(d);
+    }
+}
